d2223b5f4fa07c6b5819dada24ef123eba8c7bfa
Elena Frolova <elenschip777@gmail.com>
2019-05-30 14:15:30 +0300

    hw-04

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { HashRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import './index.css';
 import App from './components/App';
 
 ReactDOM.render(
-  <HashRouter>
-    <Route component={App} basename="/" />
-  </HashRouter>,
+  <BrowserRouter>
+    <Route component={App} />
+  </BrowserRouter>,
   document.getElementById('root'),
 );
